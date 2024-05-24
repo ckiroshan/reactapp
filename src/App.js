3f3c70e39d47ac9678a760ae03ed4c60b97235f2
Your <ckiroshan@gmail.com>
2024-05-24 17:21:12 +0530

    Used if/eles condition for conditional rendering.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import Greetings from "./components/Greetings";
-import Userprops from "./components/Userprops";
+import Password from "./components/Password";
 
 function App() {
   return (
     <>
       <h1>React Mastery begins Now!</h1>
       <Greetings />
-      <Userprops 
-      img="https://avatars.githubusercontent.com/u/117834125?v=4" 
-      name="Kane WebDev" 
-      age={18} 
-      hobbies={["Coding", "Eating", "Sleeping"]}>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi inventore provident autem ipsam, sit alias voluptatem in ad, doloribus consequuntur optio obcaecati vitae expedita labore repellendus perspiciatis voluptate illo esse?</p>
-      </Userprops>
+      <Password isValid={true} />
+      <Password isValid={false} />
     </>
   );
 }
---
+++ src/components/Password.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ValidPassword = () => <h1>Valid Password</h1>;
+const InvalidPassword = () => <h1>Invalid Password</h1>;
+
+const Password = ({ isValid }) => {
+  if (isValid) {
+    return <ValidPassword />;
+  }
+  return <InvalidPassword />;
+};
+
+export default Password;
