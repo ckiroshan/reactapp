9c0e4e835891a4bd0f70ead29b9c3403f6062421
Your <ckiroshan@gmail.com>
2024-05-24 15:30:33 +0530

    Used map() to return each object's property values.

--- src/components/Maped.jsx
+++ src/components/Maped.jsx
@@ -2,11 +2,38 @@ import React from "react";
 
 const Maped = () => {
   const nummbers = [1, 2, 3, 4, 5, 6];
+  const userInfo = [
+    {
+      username: "kane996",
+      email: "kane996@gmail.com",
+      location: "Brownsville",
+    },
+    {
+      username: "mich213",
+      email: "mich213@gmail.com",
+      location: "Amityville",
+    },
+    {
+      username: "hunter291",
+      email: "hunter291@gmail.com",
+      location: "Austinville",
+    },
+  ];
+
   return (
     <>
+      {/* Loop through an array of numbers */}
       {nummbers.map((nummber) => (
         <ul key={Math.random() * 10}>
           <li>{nummber}</li>
+        </ul>
+      ))}
+      {/* Loop through an array of Objects */}
+      {userInfo.map((user) => (
+        <ul key={Math.random() * 10}>
+          <li>{user.username}</li>
+          <li>{user.email}</li>
+          <li>{user.location}</li>
         </ul>
       ))}
     </>
