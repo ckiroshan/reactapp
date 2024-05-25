23b206c28d97713fd827f70fe521d98f6eebb767
Your <ckiroshan@gmail.com>
2024-05-25 19:32:06 +0530

    Implemented useState to change names from given array list.

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,21 +2,22 @@ import React from "react";
 import { useState } from "react";
 
 const Counter = () => {
-  const [count, SetCount] = useState(0);
+  const [username, SetUsername] = useState("John Doe");
+
+  const names = ["John Doe", "Jane Smith", "Michael Johnson", "Emily Williams", "Abigail Lewis", "Joshua Lee", "Natalie Hill", "Dylan Moore", "Hannah Baker", "Gabriel Perez", "Lily Nelson", "Benjamin Carter", "Victoria Carter", "Samuel Sanchez", "Zoe Russell", "Lucas Coleman", "Madison Coleman"];
 
   // Handlers
-  const incrementCount = () => SetCount(count + 1);
-  const decrementCount = () => SetCount(count - 1);
- 
+  const nameChanger = () => {
+    const randomName = Math.floor(Math.random() * names.length);
+    SetUsername(names[randomName]);
+  };
+
   return (
     <>
       <div className="container py-6">
-        <h1 className="text-3xl ml-14">{count}</h1>
-        <button onClick={incrementCount} className="text-2xl bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-4 mr-2 rounded">
-          +
-        </button>
-        <button onClick={decrementCount} className="text-2xl bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-4 rounded">
-          -
+        <h1 className="text-3xl ml-14">{username}</h1>
+        <button onClick={nameChanger} className="text-2xl bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-4 mr-2 rounded">
+          Change Name
         </button>
       </div>
     </>
