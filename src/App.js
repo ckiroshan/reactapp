7a2f5bda82e92e1270854d533d3efad2bd0cf3c3
Your <ckiroshan@gmail.com>
2024-05-25 14:27:49 +0530

    Added Bootstrap Navbar & simplified index.css.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import "./index.css";
+import Navigationbar from "./components/bootstrap/Navigationbar";
 import Greetings from "./components/Greetings";
+import "./index.css";
 
 function App() {
   return (
     <>
+      <Navigationbar />
       <h1>React Mastery begins Now!</h1>
       <Greetings />
     </>
