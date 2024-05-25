e1c0d395acdd2428102d3e65a049130f9b53cff8
Your <ckiroshan@gmail.com>
2024-05-25 13:19:21 +0530

    Added stylesheet to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
+import "./index.css";
 import Greetings from "./components/Greetings";
-import Cart from "./components/Cart";
 
 function App() {
   return (
     <>
       <h1>React Mastery begins Now!</h1>
       <Greetings />
-      <Cart />
     </>
   );
 }
