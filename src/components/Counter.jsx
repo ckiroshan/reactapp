6809ec3e6346a211336e9dfc4cf6da9def8f869f
Your <ckiroshan@gmail.com>
2024-05-25 19:59:32 +0530

    Implemeted useState to add, remove & update names in a list.

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,22 +2,33 @@ import React from "react";
 import { useState } from "react";
 
 const Counter = () => {
-  const [username, SetUsername] = useState("John Doe");
-
-  const names = ["John Doe", "Jane Smith", "Michael Johnson", "Emily Williams", "Abigail Lewis", "Joshua Lee", "Natalie Hill", "Dylan Moore", "Hannah Baker", "Gabriel Perez", "Lily Nelson", "Benjamin Carter", "Victoria Carter", "Samuel Sanchez", "Zoe Russell", "Lucas Coleman", "Madison Coleman"];
+  const [friends, SetFriends] = useState(["Dylan Moore", "Hannah Baker"]);
 
   // Handlers
-  const nameChanger = () => {
-    const randomName = Math.floor(Math.random() * names.length);
-    SetUsername(names[randomName]);
+  const addOne = () => {
+    SetFriends([...friends, "Benjamin Carter"]);
+  };
+  const removeOne = () => {
+    SetFriends(friends.filter((friend) => friend !== "Benjamin Carter"));
+  };
+  const updateOne = () => {
+    SetFriends(friends.map((friend) => (friend === "Benjamin Carter" ? "Victoria Carter" : friend)));
   };
 
   return (
     <>
       <div className="container py-6">
-        <h1 className="text-3xl ml-14">{username}</h1>
-        <button onClick={nameChanger} className="text-2xl bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-4 mr-2 rounded">
-          Change Name
+        {friends.map((friend) => (
+          <li key={Math.random()}>{friend}</li>
+        ))}
+        <button onClick={addOne} className="text-2xl bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-4 mr-2 rounded">
+          Add One
+        </button>
+        <button onClick={removeOne} className="text-2xl bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-4 mr-2 rounded">
+          Remove One
+        </button>
+        <button onClick={updateOne} className="text-2xl bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-4 mr-2 rounded">
+          Update One
         </button>
       </div>
     </>
