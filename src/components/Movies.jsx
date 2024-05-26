66af66fa143aba1e1046da3bf17e4c29f8beee5b
Your <ckiroshan@gmail.com>
2024-05-27 00:25:21 +0530

    Updated objects within an array using useState.

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -2,26 +2,26 @@ import React from "react";
 import { useState } from "react";
 
 const Movies = () => {
-  const [movie, SetMovie] = useState({
-    title: "Equalizer 3",
-    ratings: 7.5,
-  });
+  const [movies, SetMovies] = useState([
+    { id: 1, title: "John Wick 1", ratings: 7.5 },
+    { id: 2, title: "John Wick 2", ratings: 6 },
+    { id: 3, title: "John Wick 3", ratings: 8 },
+  ]);
 
   const handleClick = () => {
-    SetMovie({ ...movie, ratings: 10 });
+    SetMovies(movies.map((m) => (m.id === 1 ? { ...movies, title: "Jungle Beach" } : m)));
   };
 
   return (
     <>
-      <div className="container py-6">
-        <h1 className="text-3xl">
-          Movie: <span className="font-bold">{movie.title}</span>
-        </h1>
-        <p className="text-3xl">
-          Rating: <span className="font-semibold">{movie.ratings}</span>
-        </p>
+      <div className="container py-6 text-3xl">
+        {movies.map((movie) => (
+          <li className="font-medium" key={Math.random()}>
+            Movie: <span className="font-bold">{movie.title}</span>
+          </li>
+        ))}
         <button onClick={handleClick} className="text-2xl bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-4 mr-2 rounded">
-          Change Rating
+          Change Name
         </button>
       </div>
     </>
